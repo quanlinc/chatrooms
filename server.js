41bbb82cc3464396db4a11a63cffbdbf037e8024
Quanlin Chen <quanlin.chen@nwea.org>
2016-06-07 00:59:49 -0700

    config app to be run on openshift

--- server.js
+++ server.js
@@ -54,8 +54,11 @@ var server = http.createServer(function(request, response){
     serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function(){
-    console.log("Server listening on port 3000.");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
 
 var chatServer = require('./lib/chat_server');
